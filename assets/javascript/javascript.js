dcd7d32e0dafb295d0727c4d07f1245b9a34cd87
bryantuwagbale <78828750+bryantuwagbale@users.noreply.github.com>
2021-04-25 21:04:07 -0500

    final touches

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,8 +1,13 @@
-// display current date and time
-console.log(moment());
+var storedItem = localStorage.getItem("storedItem")
 
-
-function displayDate() {
-    document.getElementById("currentDay").innerHTML = 
-    Date();
+function save(){
+    var Item = document.getElementById("task").value;
+    localStorage.setItem("storedItem", Item);
+    document.getElementById("savedText").innerHTML = Item + "Saved";
 }
+
+function get(){
+    localStorage.getItem("storedItem");
+    document.getElementById("openedText").innerHTML = storedItem + " Opened";
+}
+
